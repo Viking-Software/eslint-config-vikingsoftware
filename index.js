5ced4fa68fdfa8636750e0bbe695b00b295dabd0
Brendan Rosa <bren@brenr.dev>
2021-03-19 19:01:28 -0400

    Fixed space typo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:import/errors',
         'plugin:import/warnings',
-        'plugin: jsx-a11y/recommended',
+        'plugin:jsx-a11y/recommended',
         'plugin:react-hooks/recommended',
         'airbnb'
     ],
